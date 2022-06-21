99906294fa3864e79e182ce3ae3fdd83d43f3fb9
hrozenweig <helmut.rozenweig@gmail.com>
2022-06-20 23:49:44 -0400

    test streamer service

--- src/streamer/streamer.service.spec.ts
+++ src/streamer/streamer.service.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as sinon from 'sinon';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { StreamerService } from './streamer.service';
 
 describe('StreamerService', () => {
-  let service: StreamerService;
-
-  beforeEach(async () => {
+  let streamerService: StreamerService;
+  let sandbox: sinon.SinonSandbox;
+  beforeAll(async () => {
+    sandbox = sinon.createSandbox();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StreamerService],
+      providers: [
+        StreamerService,
+        {
+          provide: getRepositoryToken(StreamerService),
+          useValue: sinon.createStubInstance(Repository),
+        },
+      ],
     }).compile();
 
-    service = module.get<StreamerService>(StreamerService);
+    streamerService = module.get<StreamerService>(StreamerService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return a promise', async () => {
+    const path = './data/deleted_products.json';
+    const getFileSpy = jest.spyOn(streamerService, 'getFile');
+    streamerService.getFile(path);
+    expect(getFileSpy).toBeDefined();
+  });
+
+  it('should return a promise', async () => {
+    const mockDbStructure: any = {
+      products: [
+        {
+          id: 'thytry-rthyrty-tryrt',
+          title: 'Tomatos - Roma',
+          vendorName: 'Adeco',
+          quantity: 50,
+          price: 2,
+          deliveryDate: '10/2/2022',
+          expiryDate: '12/12/2024',
+        },
+      ],
+      lastModifiedDate: 52
+    };
+    const path = './data/deleted_products.json';
+    const writeFileSpy = jest.spyOn(streamerService, 'getFile');
+    streamerService.writeFile(path, mockDbStructure);
+    expect(writeFileSpy).toBeDefined();
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as sinon from 'sinon';
 import { UserService } from './user.service';
-import { Repository, FindOneOptions, FindManyOptions } from 'typeorm';
+import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
